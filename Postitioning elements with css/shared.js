e7200f3577f29a021197cdae08e44b6270990d78
adarshCP <1adarshcp@gmail.com>
2022-03-29 14:31:31 +0530

    trasittion and

--- Postitioning elements with css/shared.js
+++ Postitioning elements with css/shared.js
@@ -3,13 +3,17 @@ var selectbuttons=document.querySelectorAll(".plan button");
 var modal=document.querySelector(".modal");
 var nomodalbutton=document.querySelector(".modal__action-no");
 var togglebutton=document.querySelector(".toggle-button");
-var mobilenavigation=document.querySelector(".mobile-nav")
+var mobilenavigation=document.querySelector(".mobile-nav");
+const ctaCard=document.querySelector(".main-nav__item--cta");
 
 for(var i=0;i<selectbuttons.length;i++){
     selectbuttons[i].addEventListener('click',function(){
         
         modal.classList.add("open");
-        backdrop.classList.add("open");
+        backdrop.style.display="block";
+        setTimeout(function(){ 
+            backdrop.classList.add("open");
+        },10)
     })
 };
 if(nomodalbutton){
@@ -25,10 +29,28 @@ backdrop.addEventListener("click",function(){
 function closeModal() {
     if(modal){
         modal.classList.remove("open");
-    }
-       
-        backdrop.classList.remove("open");
+    }   backdrop.classList.remove("open");
+      
+        setTimeout(function(){ 
+            backdrop.style.display="none";
+        },200);
 }
 togglebutton.addEventListener("click",function(){
     mobilenavigation.classList.add("open");
-    backdrop.classList.add("open")});
+    backdrop.style.display="block";
+    setTimeout(function(){ 
+        backdrop.classList.add("open");
+    },10)
+   });
+
+
+   ctaCard.addEventListener("animationstart",function(event){
+       console.log("Animation Started",event);
+   })
+   ctaCard.addEventListener("animationend",function(event){
+    console.log("Animation Ended",event);
+})
+ctaCard.addEventListener("animationiteration",function(event){
+    console.log("Animation Iteration",event);
+})
+
